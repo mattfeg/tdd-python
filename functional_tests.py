21cf6c4733ea1e3d6d65a13c2bbff3995b39c682
Matheus Ferreira <matheusmlfg@gmail.com>
2023-11-28 10:22:47 -0300

    Update FT according to Chapter 04 begining

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,19 +17,35 @@ class NewVisitorTest(unittest.TestCase):
 
         # User realize the page title and headline are a to-do list.
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         
         # User is invated to insert a task item immediately
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a To-Do item'
+        )
+        
         # User inserts "Buy peacock feathers" in an textbox
-
+        inputbox.send_keys('Buy peacock feathers')
+        
         # When the user press Enter, the page is refreshed,
         # and the page lists "1:Buy peacock feathers" as an item in a to-do list.
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # The page still keeps having a text box invating to insert another item.
         # The User inserts "User peacock feathers to make a fly"
-
-        # The pagme is refreshed again e shows both items in your list
+        self.fail('Finish the test!')
+        
+        # The page is refreshed again e shows both items in your list
 
         # The website generates a unique URL for the User and a small explanatory text for that.
 
